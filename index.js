4986502255313511e197d17ecd9d9705d6e847f2
Arti Verma <vermaarti930@gmail.com>
2025-11-09 18:15:43 +0530

    added login fun

--- index.js
+++ index.js
@@ -12,3 +12,23 @@ function signup(userName) {
 
 console.log(signup("john"));     // User Already Regsistered, Please Login
 console.log(signup("charlie"));  // Signup Successfull, Please Login
+
+function login(userName, password) {
+  const usernames = ["john", "alice", "bob"];
+  const correctPassword = "Emp@123";
+
+  if (usernames.includes(userName)) {
+    if (password === correctPassword) {
+      return "Login Sucessfull...";
+    } else {
+      return "Wrong Password....";
+    }
+  } else {
+    return "User Not Found, Please Signup";
+  }
+}
+
+
+console.log(login("alice", "Emp@123"));  // Login Sucessfull...
+console.log(login("alice", "1234"));     // Wrong Password....
+console.log(login("newUser", "Emp@123")); // User Not Found, Please Signup
